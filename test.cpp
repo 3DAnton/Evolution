a68addd3df5f585df22e039d2f1caedf058e8c21
3DAnton <97904300+3DAnton@users.noreply.github.com>
2022-01-17 19:36:26 +0300

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -3,15 +3,7 @@
 int main()
 {
     sf::RenderWindow window(sf::VideoMode(1000, 1000), "SFML works!");
-    sf::CircleShape octagon(1000.f);
-    sf::CircleShape shape(1000.f);
-    sf::CircleShape octagon(60.f, 8);
-    octagon.setPosition(380, 0);
-    shape.setFillColor(sf::Color::Green);
-
-    while (window.isOpen())
-    {
-        sf::Event event;
+    sf::C
         while (window.pollEvent(event))
         {
             if (event.type == sf::Event::Closed)
@@ -21,7 +13,6 @@ int main()
         window.clear();
         window.draw(shape);
         window.display();
-        window.draw(octagon); // отрисовка октагона
-    }
+        window.draw
     return 0;
 }
