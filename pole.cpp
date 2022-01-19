50a88ea25411522e07ded7f263540fe18f2b9f98
3DAnton <97904300+3DAnton@users.noreply.github.com>
2022-01-19 20:42:32 +0300

    Update pole.cpp

--- pole.cpp
+++ pole.cpp
@@ -2,5 +2,29 @@
 
 int main()
 {
+    sf::RenderWindow window(sf::VideoMode(1000, 1000), "SFML works!");
+    sf::CircleShape Crc3;
+    Crc3.setRadius(60);
+    Crc3.setPointCount(6);
+    Crc3.setOutlineColor(sf::Color(128, 0, 128));
+    Crc3.setOutlineThickness(5);//толщина границы 
+    Crc3.setFillColor(sf::Color(175, 238, 238));
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
 
+        window.clear();
+        for (int i = 0; i < 3; i++)
+        {
+            Crc3.setPosition(380+200*i, 40+i*100);
+            window.draw(Crc3);
+        }
+        window.display();
+    }
+    return 0;
 }
