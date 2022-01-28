f9e3caee79d1ecba75e0d614784c71920ab2ced0
3DAnton <97904300+3DAnton@users.noreply.github.com>
2022-01-28 17:41:43 +0300

    Update Map.cpp

--- Новая папка/proekt01/Map.cpp
+++ Новая папка/proekt01/Map.cpp
@@ -1,25 +1,30 @@
 #include "Map.h"
-#include"object.h"
 
 
-Map::getPresentation()
+Map::Map()
 {
-	std::vector<std::vector<Object::ObjectType>> result;
 
-
-
-	return result;
-}
-
-void Map::tabl(std::vector<std::vector<Object::ObjectType>> map)
-{
 	for (int i = 0; i < 10; ++i)
 	{
 		for (int j = 0; i < 10; ++j)
 		{
-			
+
 		}
 	}
 
-	//return;
+}
+
+std::vector<std::vector<Object::ObjectType>> Map::getPresentation()
+{
+	std::vector<std::vector<Object::ObjectType>> result;
+
+	for (int i = 0; i < mField.size(); ++i)
+	{
+		for (int j = 0; i < mField.size(); ++j)
+		{
+			result[i][j] = mField[i][j];
+		}
+	}
+
+	return result;
 }
