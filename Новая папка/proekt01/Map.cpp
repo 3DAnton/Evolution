90b2da4877e6f77df0af4a4300253db3e56f8e42
3DAnton <97904300+3DAnton@users.noreply.github.com>
2022-01-28 20:20:41 +0300

    Update Map.cpp

--- Новая папка/proekt01/Map.cpp
+++ Новая папка/proekt01/Map.cpp
@@ -2,11 +2,12 @@
 
 const int bot = 10;
 
-Map::Map()
+Map::Map():
+mField(20, std::vector<Object*>(32, NULL))
 {
 	for (int i = 0; i < mField.size(); ++i)
 	{
-		for (int j = 0; i < mField.size(); ++j)
+		for (int j = 0; j < mField.size(); ++j)
 		{
 			mField[i][j] = new Object(Object::ObjectType::VOID);
 		}
@@ -15,15 +16,19 @@ Map::Map()
 
 std::vector<std::vector<Object::ObjectType>> Map::getPresentation()
 {
-	std::vector<std::vector<Object::ObjectType>> result;
+	std::vector<std::vector<Object::ObjectType>> 
+		result(mField.size(), std::vector<Object::ObjectType>
+			(mField[0].size(), Object::ObjectType::NUN));
 
 	for (int i = 0; i < mField.size(); ++i)
 	{
-		for (int j = 0; i < mField.size(); ++j)
+		for (int j = 0; j < mField.size(); ++j)
 		{
-			result[i][j] = mField[i][j] ->getType();
+			result[i][j] = mField[i][j]->getType();
 		}
 	}
 
 	return result;
 }
+
+Map::~Map() {};
