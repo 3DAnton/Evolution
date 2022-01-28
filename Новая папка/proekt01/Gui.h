37942f9c652b21515120d2274c6d83c93d5c9d73
3DAnton <97904300+3DAnton@users.noreply.github.com>
2022-01-28 20:21:26 +0300

    Update Gui.h

--- Новая папка/proekt01/Gui.h
+++ Новая папка/proekt01/Gui.h
@@ -1,10 +1,22 @@
 #ifndef GUI_H
 #define GUI_H
 
+#include <vector>;
+#include "Object.h";
 
 
 class Gui
 {
+
+public:
+	Gui();
+	~Gui();
+	void draw(std::vector<std::vector<Object::ObjectType>> 
+		result);
 };
+
+
+
+
 
 #endif // !GUI_H
