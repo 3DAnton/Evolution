a225bfe1d91c2eaca17bc606b3043b2aa09b27f9
3DAnton <97904300+3DAnton@users.noreply.github.com>
2022-02-21 19:38:19 +0300

    Create God.h

---
+++ test1/hedars/God.h
@@ -0,0 +1,23 @@
+#ifndef GOD_H
+#define GOD_H
+
+#include "Gui.h"
+#include "Map.h"
+
+class God
+{
+public:
+	God();
+	~God();
+
+	void run();
+private:
+	Map mWorld; 
+	Gui mView; 
+
+	bool pause = false; 
+
+};
+
+
+#endif // GOD_H
